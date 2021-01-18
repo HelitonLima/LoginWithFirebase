6f4bd853c7829eec2b292c9a7b2d9d071426ace5
HelitonLima <helitonmartinsdelima@gmail.com>
2021-01-18 15:37:46 -0300

    Improvement components

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/intercafes/user';
-import { NavController } from '@ionic/angular';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,25 +11,42 @@ import { NavController } from '@ionic/angular';
 export class LoginPage implements OnInit {
 
   public userLogin: User = {};
+  public loading: any;
 
   constructor(
     private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
     private navCtrl: NavController
   ) { }
 
   ngOnInit() { }
 
   async login() {
+    await this.presentLoading();
+
     try {
       await this.authService.login(this.userLogin);
       console.log('login with firebase is work!!!')
     } catch (error) {
-      console.error();
+      this.presentToast(error.message);
+    } finally{
+      this.loading.dismiss();
     }
   }
 
   toRegister() {
     this.navCtrl.navigateForward('register');
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({ message: 'Aguarde...' });
+    return this.loading.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({ message, duration: 2000 });
+    toast.present();
   }
 
 }
